55b1fce1e5988228f19805a8f6db0bcb95c38274
darfux <darfux@163.com>
2016-06-29 21:22:03 +0800

    disable android sdk setup due to owner and permission issue

--- ruby_task/sub_shell/android_studio.sh
+++ ruby_task/sub_shell/android_studio.sh
@@ -4,8 +4,8 @@ if [[ "$1" == "setup" ]]; then
 	ads_package=$(find ../bin/android_studio/android-studio-ide* -type f)
 	sdk_package=$(find ../bin/android_studio/android-sdk* -type f)
 	unzip $ads_package -d /opt/
-	tar -xzvf $sdk_package -C /opt/android-studio
-	mv /opt/android-studio/android-sdk-linux /opt/android-studio/sdk
+	#tar -xzvf $sdk_package -C /opt/android-studio
+	#mv /opt/android-studio/android-sdk-linux /opt/android-studio/sdk
 	cp ../bin/android_studio/jetbrains-studio.desktop /usr/share/applications/
-	echo "export PATH=\"\$PATH:/opt/android-studio/sdk/platform-tools\"" >> ~/.profile
+	#echo "export PATH=\"\$PATH:/opt/android-studio/sdk/platform-tools\"" >> ~/.profile
 fi
