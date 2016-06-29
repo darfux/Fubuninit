eee1e26b57215904308f595387a8e2e83e67a6e9
darfux <darfux@163.com>
2016-06-29 20:49:08 +0800

    minor fixes

--- ruby_task/config_favorite.rb
+++
@@ -1,3 +0,0 @@
-def config_favorite
-	run_bash_file("modify_theme", "favorite")
-end
--- ruby_task/lib/fubuninit.rb
+++ ruby_task/lib/fubuninit.rb
@@ -1,17 +1,19 @@
 module Fubuninit
 	TMP_DIR = '/tmp/fubuninit'
-	LOG_DIR = '/var/fubuninit'
+	VAR_DIR = '/var/fubuninit'
+	LOG_DIR  = VAR_DIR
 end
 
 if !File.exists?(Fubuninit::TMP_DIR)
 	Dir.mkdir Fubuninit::TMP_DIR
 end
 
-if !File.exists?(Fubuninit::LOG_DIR)
-	Dir.mkdir Fubuninit::LOG_DIR
+if !File.exists?(Fubuninit::VAR_DIR)
+	Dir.mkdir Fubuninit::VAR_DIR
 end
 
 `chmod 777 #{Fubuninit::TMP_DIR}`
 `chmod 777 #{Fubuninit::LOG_DIR}`
 ENV["FUB_TMP_DIR"] = Fubuninit::TMP_DIR
-ENV["FUB_LOG_DIR"] = Fubuninit::TMP_DIR
+ENV["FUB_VAR_DIR"] = Fubuninit::VAR_DIR
+ENV["FUB_LOG_DIR"] = Fubuninit::LOG_DIR
