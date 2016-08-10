64e61948fbb823bf9896c26edfe1dd51610242e7
darfux <darfux@163.com>
2016-08-10 10:10:06 +0800

    fix missing -y in install_ruby

--- install_ruby.sh
+++ install_ruby.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install curl
+sudo apt-get -y install curl
 
 proxychains gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 # With proxy:
