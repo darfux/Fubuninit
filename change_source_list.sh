2588da8d1fd08130773eedd38ce57fb75bbae351
darfux <darfux@163.com>
2018-07-10 13:05:55 +0800

    Update source list

--- change_source_list.sh
+++ change_source_list.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-UBUNTU_VERSION=xenial
+UBUNTU_VERSION=bionic
 
 sources_name=$UBUNTU_VERSION.sources.list
 
 cp ./sources.list.template ./$sources_name
 
-sed -i "s/{UBUNTU_VERSION}/xenial/g"  ./$sources_name
+sed -i "s/{UBUNTU_VERSION}/$UBUNTU_VERSION/g"  ./$sources_name
 
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
 sudo mv ./$sources_name /etc/apt/sources.list
